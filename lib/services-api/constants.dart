df1ecb5d1396ea4a497450eaa4e4b1467458806e
Thông Nguyễn Minh <thongnm11@fpt.com.vn>
2025-12-04 09:51:58 +0700

    Update Code

--- lib/services-api/constants.dart
+++ lib/services-api/constants.dart
@@ -3,5 +3,7 @@
 // 🔴 QUAN TRỌNG: Thay thế bằng IP của máy tính bạn.
 // Ví dụ: http://192.168.1.10:3000
 // Hoặc 10.0.2.2:3000 nếu dùng Android Emulator.
-const String baseUrl = 'http://10.0.2.2:3000'; 
+// IP IOS Emulator: 127.0.0.1:54474
+// IP Sever-PC: http://10.0.0.108:3000
+const String baseUrl = 'http://10.0.0.108:3000';
 const String authEndpoint = '$baseUrl/api/auth';
